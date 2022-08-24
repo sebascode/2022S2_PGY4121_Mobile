4610e0bfdd27babdcddd93abfcf207c9df676c59
Sebas Contreras <swf.sebas@gmail.com>
2022-08-24 12:50:08 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '2022S2_PGY4121_Mobile',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
