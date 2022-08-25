d9fcb02b27e7d14c5ccc88b0c529710e98194434
Sebas Contreras <swf.sebas@gmail.com>
2022-08-24 21:17:17 -0400

    cambios

--- src/app/tablinks/tablinks-routing.module.ts
+++ src/app/tablinks/tablinks-routing.module.ts
@@ -5,8 +5,32 @@ import { TablinksPage } from './tablinks.page';
 
 const routes: Routes = [
   {
+    path: 'tablinks',
+    component: TablinksPage,
+    children: [
+      {
+        path: 'perfil',
+        loadChildren: () => import('../perfil/perfil.module').then(m => m.PerfilPageModule)
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('../dashboard/dashboard.module').then(m => m.DashboardPageModule)
+      },
+      {
+        path: 'configuraciones',
+        loadChildren: () => import('../configuraciones/configuraciones.module').then(m => m.ConfiguracionesPageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/tablinks/perfil',
+        pathMatch: 'full'
+      },
+    ]
+  },
+  {
     path: '',
-    component: TablinksPage
+    redirectTo: 'tablinks/perfil',
+    pathMatch: 'full'
   }
 ];
 
