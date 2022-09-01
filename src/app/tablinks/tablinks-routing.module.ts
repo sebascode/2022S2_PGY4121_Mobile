88faffe6f3eed65b1ea6aa78b37f30a754067617
Sebas Contreras <swf.sebas@gmail.com>
2022-08-31 20:21:39 -0400

    cambio tabs

---
+++ src/app/tablinks/tablinks-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TablinksPage } from './tablinks.page';
+
+const routes: Routes = [
+  {
+    path: 'tablinks',
+    component: TablinksPage,
+    children: [
+      {
+        path: 'perfil',
+        loadChildren: () => import('../perfil/perfil.module').then( m => m.PerfilPageModule)
+      },
+      {
+        path: 'inicio',
+        loadChildren: () => import('../inicio/inicio.module').then( m => m.InicioPageModule)
+      },
+      {
+        path: 'configuracion',
+        loadChildren: () => import('../configuracion/configuracion.module').then( m => m.ConfiguracionPageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/tablinks/inicio',
+        pathMatch: 'full'
+      },
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tablinks/inicio',
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TablinksPageRoutingModule {}
